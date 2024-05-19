b2881a6d19b9769e8ac7c5c4fa2d8e8dcb09959a
luizcostaxp <luizcostaxp@gmail.com>
2024-05-19 18:00:49 -0300

    conteúdo JS

--- interactivecard.js
+++ interactivecard.js
@@ -36,8 +36,90 @@ function setCardMonth(e){
     cardMonth.innerText=format(e.target.value);
 }
 
+function setCardYear(e){
+    cardYear.innerText=format(e.target.value);
+}
+
+function setCardCvc(e){
+    cardCvc.innerText=format(e.target.value);
+}
+
+function format(s){
+    return s.toString().replace(/\d{4}(?=.)/g,"$&");
+}
+
+function handleSubmit(e){
+    e.preventDefault();
+    if(!inputName.value){
+        inputName.classList.add("error");
+        inputName.parentElement.classList.add("error-message");
+    }
+    else{
+        inputName.classList.remove("error");
+        inputName.parentElement.classList.remove("error-message");
+    }
+
+    if(!inputNumber.value){
+        inputNumber.classList.add("error");
+        inputNumber.parentElement.classList.add("error-message");
+    }
+    else if(inputNumber.value.length<16)
+    {
+        inputNumber.classList.add("error");
+    }
+    else{
+        inputNumber.classList.remove("error");
+        inputNumber.parentElement.classList.remove("error-message");
+    }
+
+    if(!inputMonth.value)
+    {
+        inputMonth.classList.add("error");
+        inputMonth.parentElement.classList.add("error-message");
+    }
+    else{
+        inputMonth.classList.remove("error");
+        inputMonth.parentElement.classList.remove("error-message");
+    }
+
+    if(!inputYear.value)
+    {
+        inputYear.classList.add("error");
+        inputYear.parentElement.classList.add("error-message");
+    }
+    else{
+        inputYear.classList.remove("error");
+        inputYear.parentElement.classList.remove("error-message");
+    }
+
+    if(!inputCvc.value){
+        inputCvc=classList.add("error");
+        inputCvc.parentElement.classList.add("error-message");
+    }
+    else{
+        inputCvc.classList.remove("error");
+        inputCvc.parentElement.classList.remove("error-message");        
+    }
+
+    if(
+        inputName.value&&
+        inputNumber.value&&
+        inputMonth.value&&
+        inputYear.value&&
+        inputCvc.value&&
+        inputNumber.value.length==16
+    )
+    {
+        done.classList.remove("hidden");
+        form.classList.add("hidden");
+    }
+
+    }
+
+submitBtn.addEventListener("click", handleSubmit);
 
 inputNumber.addEventListener("keyup", setCardNumber);
 inputName.addEventListener("keyup", setCardName);
 inputMonth.addEventListener("keyup", setCardMonth);
-
+inputYear.addEventListener("keyup", setCardYear);
+inputCvc.addEventListener("keyup", setCardCvc);
